c00947a6f6a4f60012a1114b08cca59bdef61c0b
laethitia <lalatisha@gmail.com>
2017-04-11 20:54:48 -0400

    Init files

---
+++ index.js
@@ -0,0 +1,20 @@
+function add() {
+	//console.log("let's add");
+	var lp=1+2;
+	console.log(lp);
+}
+function subtract() {
+	//console.log("let's subtract");
+	var sm=1-2;
+	console.log(sm);
+}
+function divide() {
+	//console.log("let's divide");
+	var pl=6/2;
+	console.log(pl);
+}
+function multiply() {
+	//console.log("let's multiply");
+	var ms=1*4;
+	console.log(ms);
+}
